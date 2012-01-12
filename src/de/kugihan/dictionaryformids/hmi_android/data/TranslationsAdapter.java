53fcfe044ef471c32e783d6d91a9d225b1932fbc
a-xin <a-xin@4c697282-65e8-472e-bc23-9cf21df0447e>
2012-01-12 00:47:33 +0000

    refactored filter into translation adapter for search as you type
    git-svn-id: https://dictionarymid.svn.sourceforge.net/svnroot/dictionarymid/trunk/Android@422 4c697282-65e8-472e-bc23-9cf21df0447e

--- src/de/kugihan/dictionaryformids/hmi_android/data/TranslationsAdapter.java
+++ src/de/kugihan/dictionaryformids/hmi_android/data/TranslationsAdapter.java
@@ -7,7 +7,10 @@
  ******************************************************************************/
 package de.kugihan.dictionaryformids.hmi_android.data;
 
+import java.util.Observable;
+
 import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,27 +18,94 @@ import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Filter;
+import android.widget.Filterable;
 import de.kugihan.dictionaryformids.dataaccess.DictionaryDataFile;
+import de.kugihan.dictionaryformids.general.DictionaryException;
+import de.kugihan.dictionaryformids.hmi_android.DictionaryForMIDs;
 import de.kugihan.dictionaryformids.hmi_android.Preferences;
 import de.kugihan.dictionaryformids.hmi_android.R;
 import de.kugihan.dictionaryformids.hmi_android.view_helper.SingleTranslationViewHelper;
 import de.kugihan.dictionaryformids.translation.SingleTranslationExtension;
+import de.kugihan.dictionaryformids.translation.TranslationExecution;
 import de.kugihan.dictionaryformids.translation.TranslationExecutionCallback;
+import de.kugihan.dictionaryformids.translation.TranslationParameters;
 import de.kugihan.dictionaryformids.translation.TranslationResult;
 
 /**
  * TranslationsAdapter handles the data for the translation list.
  *
  */
-public class TranslationsAdapter extends BaseAdapter implements
-		TranslationExecutionCallback {
+public class TranslationsAdapter extends BaseAdapter implements Filterable {
 
 	/**
 	 * The current TranslationResult represented by this adapter.
 	 */
 	private TranslationResult data = new TranslationResult();
 
-	public TranslationsAdapter() {
+	/**
+	 * Filter used to search the dictionary.
+	 */
+	private AutoCompleteFilter filter = null;
+
+	/**
+	 * True if the filter should keep waiting for the dictionary search result.
+	 */
+	private boolean shouldFilterWait = true;
+
+	/**
+	 * Observable used to publish changes to the state of the filter.
+	 */
+	private final FilterObservable filterStateObservable = new FilterObservable();
+
+	/**
+	 * Parameters that are used by filter.
+	 */
+	private TranslationParameters parameters = null;
+
+	public TranslationsAdapter(final TranslationParameters parameters) {
+		setTranslationParameters(parameters);
+	}
+
+	/**
+	 * Sets the parameter used for the next translation.
+	 *
+	 * @param parameters
+	 *            the parameters used for the next translation
+	 */
+	public void setTranslationParameters(final TranslationParameters parameters) {
+		this.parameters = parameters;
+	}
+
+	/**
+	 * Cancels any active filtering operation by interrupting the active
+	 * translation thread.
+	 */
+	public void cancelActiveFilter() {
+		TranslationExecution.cancelLastTranslation();
+		synchronized (getFilter()) {
+			shouldFilterWait = false;
+			// manually notify the thread as a cancelled translation thread does
+			// not receive a callback
+			getFilter().notifyAll();
+		}
+	}
+
+	/**
+	 * Gets the last translation result or an empty translation result.
+	 *
+	 * @return the last translation result or an empty translation result
+	 */
+	public TranslationResult getData() {
+		return data;
+	}
+
+	/**
+	 * Clears the translation results.
+	 */
+	public void clearData() {
+		data = new TranslationResult();
+		notifyDataSetInvalidated();
 	}
 
 	/**
@@ -122,18 +192,170 @@ public class TranslationsAdapter extends BaseAdapter implements
 	 * {@inheritDoc}
 	 */
 	@Override
-	public final void deletePreviousTranslationResult() {
-		data = new TranslationResult();
-		notifyDataSetChanged();
+	public Filter getFilter() {
+		if (filter == null) {
+			filter = new AutoCompleteFilter(filterStateObservable);
+		}
+		return filter;
 	}
 
 	/**
-	 * {@inheritDoc}
+	 * Returns if there currently is an active filtering thread.
+	 *
+	 * @return true if there is an active filtering thread
 	 */
-	@Override
-	public final void newTranslationResult(
-			final TranslationResult resultOfTranslation) {
-		data = resultOfTranslation;
-		notifyDataSetChanged();
+	public boolean isFilterActive() {
+		return filter != null && filter.isActive;
+	}
+
+	/**
+	 * Returns the handle to the filter state observable.
+	 *
+	 * @return the handle to the filter state observable
+	 */
+	public Observable getFilterStateObservable() {
+		return filterStateObservable;
+	}
+
+	private static class FilterObservable extends Observable {
+		@Override
+		protected void setChanged() {
+			super.setChanged();
+		}
+	}
+
+	private class AutoCompleteFilter extends Filter implements TranslationExecutionCallback {
+
+		/**
+		 * Saves if there currently is an active filter thread.
+		 */
+		private boolean isActive = false;
+
+		/**
+		 * Observable that receives state changes.
+		 */
+		private final FilterObservable stateChangeObservable;
+
+		/**
+		 * Class variable to share result of translation between the synchronous
+		 * callback to newTranslationResult() and performFiltering().
+		 */
+		private TranslationResult translationResult = new TranslationResult();
+
+		/**
+		 * Creates a new instance of the filter providing an observable.
+		 *
+		 * @param stateObservable
+		 *            the observable used for publishing state changes to
+		 *            isActive
+		 */
+		public AutoCompleteFilter(final FilterObservable stateObservable) {
+			if (stateObservable == null) {
+				throw new NullPointerException();
+			}
+			this.stateChangeObservable = stateObservable;
+		}
+
+		@Override
+		protected FilterResults performFiltering(CharSequence constraint) {
+
+			synchronized (this) {
+				shouldFilterWait = true;
+			}
+
+			setState(true);
+
+			final FilterResults result = new FilterResults();
+			result.values = new TranslationResult();
+			result.count = 0;
+
+			if (parameters == null) {
+				return result;
+			}
+			if (DictionaryDataFile.numberOfAvailableLanguages == 0) {
+				return result;
+			}
+
+			final String searchString = constraint.toString().trim();
+			final StringBuffer searchWord = new StringBuffer(searchString);
+			if (searchWord.length() == 0) {
+				return result;
+			}
+
+			if (!DictionaryForMIDs.hasSearchModifiers(searchWord)) {
+				DictionaryForMIDs.makeWordMatchBeginning(searchWord);
+			}
+
+			parameters.executeInBackground = true;
+			parameters.toBeTranslatedWordText = searchWord.toString();
+			TranslationExecution.setTranslationExecutionCallback(this);
+
+			try {
+				TranslationExecution.executeTranslation(parameters);
+			} catch (DictionaryException e) {
+				return result;
+			}
+
+			// wait for background thread to finish translation
+			synchronized (this) {
+				try {
+					while (shouldFilterWait) {
+						wait();
+					}
+				} catch (InterruptedException e) {
+					// return null to indicate an interruption
+					return null;
+				}
+			}
+
+			// callback to newTranslationResults changed translationResult
+
+			result.values = translationResult;
+			result.count = translationResult.numberOfFoundTranslations();
+
+			return result;
+		}
+
+		@Override
+		protected void publishResults(CharSequence constraint, FilterResults results) {
+			setState(false);
+
+			// null indicates translation was interrupted
+			if (results == null) {
+				// if interrupted do nothing
+				return;
+			}
+
+			if (results.values == null) {
+				data = new TranslationResult();
+				notifyDataSetInvalidated();
+				Log.d(DictionaryForMIDs.LOG_TAG, "Invalidated");
+			} else {
+				data = (TranslationResult) results.values;
+				notifyDataSetChanged();
+				Log.d(DictionaryForMIDs.LOG_TAG, "Changed");
+			}
+		}
+
+		@Override
+		public void deletePreviousTranslationResult() {
+			// ignore this event
+			// as we only update the view when the new result is available
+		}
+
+		@Override
+		public void newTranslationResult(TranslationResult resultOfTranslation) {
+			translationResult = resultOfTranslation;
+			synchronized (this) {
+				shouldFilterWait = false;
+				notifyAll();
+			}
+		}
+
+		private void setState(boolean state) {
+			isActive = state;
+			stateChangeObservable.setChanged();
+			stateChangeObservable.notifyObservers(isActive);
+		}
 	}
 }
