626f1a13b6ac5cf50cef9e2ac2ac22807d5febe5
a-xin <a-xin@f4d0a3ea-2ba0-4f8e-b77a-cc5a5f000db1>
2014-07-12 11:47:39 +0000

    Migrate preferences of previous version and fix restoring of hidden dictionaries into preferences

--- trunk/Android/src/de/kugihan/dictionaryformids/hmi_android/data/Dictionary.java
+++ trunk/Android/src/de/kugihan/dictionaryformids/hmi_android/data/Dictionary.java
@@ -1,10 +1,18 @@
 package de.kugihan.dictionaryformids.hmi_android.data;
 
-import java.util.Arrays;
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashSet;
 import java.util.Observable;
 
 import de.kugihan.dictionaryformids.dataaccess.DictionaryDataFile;
+import de.kugihan.dictionaryformids.dataaccess.LanguageDefinition;
+import de.kugihan.dictionaryformids.hmi_android.DictionaryForMIDs;
 import de.kugihan.dictionaryformids.hmi_android.Preferences;
 
 public class Dictionary extends Observable {
@@ -14,6 +22,11 @@ public class Dictionary extends Observable {
 	private final Preferences.DictionaryType type;
 	private final String path;
 	private String abbreviation;
+
+	/**
+	 * Holds a language selection set when the dictionary has not been loaded yet.
+	 */
+	private LanguageSelectionSet languageSelectionSet = null;
 
 	public static class LanguagePair {
 		private final int fromLanguage;
@@ -38,11 +51,149 @@ public class Dictionary extends Observable {
 		}
 	}
 
+	public abstract static class LanguageSelectionSet {
+		protected static final String JSON_TYPE_PROPERTY = "type";
+		public abstract void applyToDictionary(Dictionary dictionary);
+		public abstract JSONObject serializeAsJson() throws JSONException;
+		public static LanguageSelectionSet unserialize(JSONObject jsonObject) throws JSONException {
+			// Check if a selection was saved
+			if (jsonObject.get(JSON_TYPE_PROPERTY) == null
+					|| jsonObject.getString(JSON_TYPE_PROPERTY).length() == 0) {
+				return new LanguagePairSelectionSet(new LanguagePair[0]);
+			}
+
+			// Try to create a selection object
+			LanguageIndexSelectionSet indexSelection = LanguageIndexSelectionSet.createFromJson(jsonObject);
+			if (indexSelection != null) {
+				return indexSelection;
+			}
+			LanguageSelectionSet pairSelection = LanguagePairSelectionSet.createFromJson(jsonObject);
+			if (pairSelection != null) {
+				return pairSelection;
+			}
+
+			throw new JSONException("Unsupported selection type: " + jsonObject.get(JSON_TYPE_PROPERTY));
+		}
+	}
+
+	public static class LanguagePairSelectionSet extends LanguageSelectionSet {
+		public static final String JSON_TYPE_VALUE = "pairs";
+		public final LanguagePair[] languagePairs;
+
+		public LanguagePairSelectionSet(LanguagePair[] languagePairs) {
+			this.languagePairs = languagePairs;
+		}
+
+		@Override
+		public void applyToDictionary(Dictionary dictionary) {
+			if (dictionary.getFile() == null) {
+				throw new IllegalStateException("Dictionary must be loaded first");
+			}
+			for (Dictionary.LanguagePair selectedPair : languagePairs) {
+				dictionary.setPairSelection(selectedPair.getFromLanguage(), selectedPair.getToLanguage(), true);
+			}
+		}
+
+		@Override
+		public JSONObject serializeAsJson() throws JSONException {
+			// Create array of selected language pairs
+			final JSONArray jsonArraySelectionPairs = new JSONArray();
+			for (Dictionary.LanguagePair languagePair : languagePairs) {
+				JSONObject languagePairJsonObject = new JSONObject();
+				languagePairJsonObject.put("from", languagePair.getFromLanguage());
+				languagePairJsonObject.put("to", languagePair.getToLanguage());
+				jsonArraySelectionPairs.put(languagePairJsonObject);
+			}
+			final JSONObject jsonResult = new JSONObject();
+			jsonResult.put(JSON_TYPE_PROPERTY, JSON_TYPE_VALUE);
+			jsonResult.put("array", jsonArraySelectionPairs);
+			return jsonResult;
+		}
+
+		public static LanguagePairSelectionSet createFromJson(JSONObject jsonObject) throws JSONException {
+			boolean isPairSelection = LanguagePairSelectionSet.JSON_TYPE_VALUE.equals((jsonObject.getString(JSON_TYPE_PROPERTY)));
+			if (!isPairSelection) {
+				return null;
+			}
+			JSONArray languagePairsJsonArray = jsonObject.getJSONArray("array");
+			LanguagePair[] languagePairs = new LanguagePair[languagePairsJsonArray.length()];
+			for (int j = 0; j < languagePairsJsonArray.length(); j++) {
+				JSONObject languagePairJsonObject = languagePairsJsonArray.getJSONObject(j);
+				int from = languagePairJsonObject.getInt("from");
+				int to = languagePairJsonObject.getInt("to");
+				languagePairs[j] = new LanguagePair(from, to);
+			}
+			return new LanguagePairSelectionSet(languagePairs);
+		}
+	}
+
+	public static class LanguageIndexSelectionSet extends LanguageSelectionSet {
+		public static final String JSON_TYPE_VALUE = "index";
+		private final int selectedIndex;
+
+		public LanguageIndexSelectionSet(int selectedIndex) {
+			this.selectedIndex = selectedIndex;
+		}
+
+		@Override
+		public void applyToDictionary(Dictionary dictionary) {
+			if (dictionary.getFile() == null) {
+				throw new IllegalStateException("Dictionary must be loaded first");
+			}
+			final DictionaryDataFile file = dictionary.getFile();
+			final int numberOfTargetLanguages = file.numberOfAvailableLanguages - 1;
+			int numberOfSearchableLanguages = 0;
+			int fromLanguage;
+			for (fromLanguage = 0; fromLanguage < file.supportedLanguages.length; fromLanguage++) {
+				LanguageDefinition supportedLanguage = file.supportedLanguages[fromLanguage];
+				if (supportedLanguage.isSearchable) {
+					if (selectedIndex < (numberOfSearchableLanguages + 1) * numberOfTargetLanguages) {
+						break;
+					}
+					numberOfSearchableLanguages++;
+				}
+			}
+			int toLanguage = selectedIndex % numberOfTargetLanguages;
+			if (fromLanguage >= file.numberOfAvailableLanguages) {
+				Log.i(DictionaryForMIDs.LOG_TAG, "Invalid language selection index (higher than available selections): " + selectedIndex);
+				return;
+			}
+
+			// Add one to language as the diagonal is not selectable (Language0 -> Language0)
+			if (toLanguage >= fromLanguage) {
+				toLanguage++;
+			}
+			dictionary.setPairSelection(fromLanguage, toLanguage, true);
+		}
+
+		@Override
+		public JSONObject serializeAsJson() throws JSONException {
+			final JSONObject jsonObject = new JSONObject();
+			jsonObject.put(JSON_TYPE_PROPERTY, JSON_TYPE_VALUE);
+			jsonObject.put("index", selectedIndex);
+			return jsonObject;
+		}
+
+		public static LanguageIndexSelectionSet createFromJson(JSONObject jsonObject) throws JSONException {
+			boolean isIndexSelection = LanguageIndexSelectionSet.JSON_TYPE_VALUE.equals((jsonObject.getString(JSON_TYPE_PROPERTY)));
+			if (!isIndexSelection) {
+				return null;
+			}
+			int index = jsonObject.getInt("index");
+			return new LanguageIndexSelectionSet(index);
+		}
+	}
+
 	public Dictionary(String abbreviation, Preferences.DictionaryType type, String path) {
+		this(abbreviation, type, path, null);
+	}
+
+	public Dictionary(String abbreviation, Preferences.DictionaryType type, String path, LanguageSelectionSet languageSelectionSet) {
 		this.file = null;
 		this.type = type;
 		this.path = path;
 		this.abbreviation = abbreviation;
+		this.languageSelectionSet = languageSelectionSet;
 	}
 
 	public Dictionary(DictionaryDataFile file, String abbreviation, Preferences.DictionaryType type, String path) {
@@ -52,11 +203,8 @@ public class Dictionary extends Observable {
 		this.abbreviation = abbreviation;
 	}
 
-	public Dictionary(DictionaryDataFile file, Preferences.DictionaryType type, String path, LanguagePair[] selectedPairs) {
+	public Dictionary(DictionaryDataFile file, Preferences.DictionaryType type, String path) {
 		this(file, file.dictionaryAbbreviation, type, path);
-		if (selectedPairs != null) {
-			this.selectedPairs.addAll(Arrays.asList(selectedPairs));
-		}
 	}
 
 	public DictionaryDataFile getFile() {
@@ -69,11 +217,25 @@ public class Dictionary extends Observable {
 		}
 		this.file = file;
 		this.abbreviation = file.dictionaryAbbreviation;
+		if (languageSelectionSet != null) {
+			languageSelectionSet.applyToDictionary(this);
+		}
+		setChanged();
+		notifyObservers();
+	}
+
+	public void setLanguageSelectionSet(LanguageSelectionSet selectionSet) {
+		if (file != null) {
+			throw new IllegalStateException("LanguageSet can only be set if file is not active.");
+		}
+		this.languageSelectionSet = selectionSet;
 		setChanged();
 		notifyObservers();
 	}
 
 	public void unloadFile() {
+		String languages = TextUtils.join(" ", getLanguages());
+		this.abbreviation = languages;
 		this.file = null;
 		this.selectedPairs.clear();
 		setChanged();
@@ -109,6 +271,9 @@ public class Dictionary extends Observable {
 	}
 
 	public void setPairSelection(int fromLanguage, int toLanguage, boolean selected) {
+		// Reset saved selection
+		languageSelectionSet = null;
+		// Update selection state
 		LanguagePair pair = getSelectedPair(fromLanguage, toLanguage);
 		if (selected && pair == null) {
 			selectedPairs.add(new LanguagePair(fromLanguage, toLanguage));
@@ -117,6 +282,15 @@ public class Dictionary extends Observable {
 		}
 		setChanged();
 		notifyObservers();
+	}
+
+	public LanguageSelectionSet getSelectedLanguages() {
+		if (languageSelectionSet != null) {
+			return languageSelectionSet;
+		} else {
+			LanguagePairSelectionSet selection = new LanguagePairSelectionSet(getSelectedPairs());
+			return selection;
+		}
 	}
 
 	public LanguagePair[] getSelectedPairs() {
